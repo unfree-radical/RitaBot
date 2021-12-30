0b4341eab66656108a5aa681bf56411aab46bb2f
Zycore <admin@zycore.co.uk>
2021-12-30 18:28:50 +0000

    Patch

--- src/core/lang.check.js
+++ src/core/lang.check.js
@@ -5,6 +5,7 @@
 
 // Codebeat:disable[LOC,ABC,BLOCK_NESTING,ARITY]
 /* eslint-disable sort-keys */
+/* eslint-disable prefer-object-has-own */
 const translate = require("rita-google-translate-api");
 const ISO6391 = require("iso-639-1");
 const fn = require("./helpers");
